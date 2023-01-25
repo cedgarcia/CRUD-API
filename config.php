961cb7e5c72869dd6046878e6ec4ddad9b38584e
Cedrick Garcia <cedrick.garcia@ippphilippines.com>
2023-01-25 13:06:44 +0800

    connect to db(PDO)

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// $host = "localhost";
+// $username = "root";
+// $pass = "";
+// $db = "todo-db";
+// Connect to the database
+// try{
+// $pdo = new PDO('mysql:host=$host;dbname=$db', $username, $pass);
+
+// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// echo "Connected successfully";
+// } catch(PDOException $e) {
+// echo "Connection failed: " . $e->getMessage();
+// }
+
+
+$host = "localhost";
+$username = "root";
+$pass = "";
+
+try {
+  $conn = new PDO("mysql:host=$host;dbname=todo-db", $username, $pass);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+// ?>
